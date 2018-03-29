7c32f8c20aa9df5a5751bf5cc3f9d343be59cdcf
williexu <wx44@cornell.edu>
2018-03-29 15:07:20 -0700

    linter

--- scripts/ci/verify_linter.sh
+++ scripts/ci/verify_linter.sh
@@ -2,16 +2,12 @@
 
 set -e
 
-# build packages
 . $(cd $(dirname $0); pwd)/artifacts.sh
 
 ls -la $share_folder/build
 
 ALL_MODULES=`find $share_folder/build/ -name "*.whl"`
 
-# title 'Install azdev'
-pip install -qqq -e ./tools
+pip install -e ./tools
 [ -d privates ] && pip install -qqq privates/*.whl
-pip install -qqq $ALL_MODULES
-
-azdev cli-lint
+pip install $ALL_MODULES
