2884652111e2217c21424a7366982f405cc924b5
williexu <wx44@cornell.edu>
2019-02-27 12:14:11 -0800

    test

--- scripts/ci/verify_linter.sh
+++ scripts/ci/verify_linter.sh
@@ -7,9 +7,13 @@ set -ev
 ls -la $share_folder/build
 
 ALL_MODULES=`find $share_folder/build/ -name "*.whl"`
-echo ALL_MODULES
+echo $ALL_MODULES
 pip install -e ./tools
 [ -d privates ] && pip install -qqq privates/*.whl
 pip install $ALL_MODULES
+
+az ad user create -h
+az -v
+pip list
 
 azdev cli-lint --ci
